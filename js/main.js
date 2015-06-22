07d52a53d3cbc8ef71ee8c1b4670d35bb564fe29
Branko Djurkovic <brankko@gmail.com>
2015-06-22 17:21:10 +0200

    jquery form validation

--- js/main.js
+++ js/main.js
@@ -1,1 +1,71 @@
+$(document).ready(function(){
+	
+	$("#licence").click(function() {
+		if ($("#licence").is(':checked')) {
+			$("#register").attr('disabled', false);
+		} else {
+			$("#register").attr('disabled', true);
+		}
+	});
 
+
+	$("#register").click(function(event){
+		event.preventDefault();
+
+		var formIsValid = true;
+
+
+		if ($("#firstname").val() === '') {
+			formIsValid = false;
+			$("#firstname").addClass('error');
+		} else {
+			$("#firstname").removeClass('error');
+		}
+
+		if ($("#lastname").val() === '') {
+			formIsValid = false;
+			$("#lastname").addClass('error');
+		} else {
+			$("#lastname").removeClass('error');
+		}
+
+		if ($("#email").val().indexOf("@") === -1) {
+			formIsValid = false;
+			$("#email").addClass('error');
+		} else {
+			$("#email").removeClass('error');
+		}
+
+		if ($("#password").val().length < 6) {
+			formIsValid = false;
+			$("#password").addClass('error');
+		} else {
+			$("#password").removeClass('error');
+		}
+
+		if ($("#password2").val() !== $("#password").val()) {
+			formIsValid = false;
+			$("#password2").addClass('error');
+		} else {
+			$("#password2").removeClass('error');
+		}
+
+		if (isNaN($("#donation").val())) {
+			formIsValid = false;
+			$("#donation").addClass('error');
+		} else {
+			$("#donation").removeClass('error');
+		}
+
+		if(formIsValid) {
+			console.info("Everything is okay!");
+		} else {
+			console.info("Form is okay... NOT!");
+		}
+
+		return false;
+
+	});
+
+
+});
